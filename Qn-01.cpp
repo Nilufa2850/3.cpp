98c6384d9e60fc845b5a3609b89d4d5e1b4a2326
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 22:14:10 +0530

    Create Qn-01.cpp

---
+++ Qn-01.cpp
@@ -0,0 +1,14 @@
+//WAP in c++ to calculate an average of 3 numbers.
+
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int x, y , z ;
+
+    cout<<"Enter three no.s : " ;
+    cin>>x>>y>>z ;
+
+    cout<<"Average is "<<(x+y+z)/3 ;
+    return 0 ;
+}
