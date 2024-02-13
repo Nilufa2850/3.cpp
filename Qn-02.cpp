608044dc2d5826f98f2f9cf4b5f53d2a498e0e0d
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-13 22:16:48 +0530

    Create Qn-02.cpp

---
+++ Qn-02.cpp
@@ -0,0 +1,14 @@
+//WAP in c++ to calculate Square of a number.
+
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int x ;
+
+    cout<<"Enter a number : " ;
+    cin>>x ;
+
+    cout<<"Square of "<<x<<"is "<<x*x ;
+    return 0 ;
+}
