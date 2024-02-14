a9f6b3dba24b18757f74aeadf0c003c52f0795b1
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-02-14 11:59:57 +0530

    Create Qn-04.cpp

---
+++ Qn-04.cpp
@@ -0,0 +1,14 @@
+//WAP in c++ to find the max of two numbers.
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int x , y ;
+    cout<<"Enter two numbers : " ;
+    cin>>x>>y ;
+    if (x>y)
+        cout<<"x="<<x<<" is max";
+    else
+        cout<<"y="<<y<<" is max" ;
+    return 0 ;
+}
